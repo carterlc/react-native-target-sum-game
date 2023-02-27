fe7d1348be0c6a4790100ef8a2b5b26623275c30
Carter Clifford <carterlc@live.ca>
2023-02-27 12:37:50 -0600

    Added a randomNumberCount of 6 to App.js to have 6 random numbers that can potentially be added to targetSum and added an array of 6 new random numbers that will stay under the targetSum in Game.js

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ import Game from './Game';
 class App extends React.Component {
     render() {
         return (
-            <Game />
+            <Game randomNumberCount={6} />
         );
     }
 }
--- Game.js
+++ Game.js
@@ -8,14 +8,26 @@
 
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import { View, Text, StyleSheet } from 'react-native';
 
 class Game extends React.Component {
-    target = 10 + Math.floor(40 * Math.random());
+    static propTypes = {
+        randomNumberCount: PropTypes.string.isRequired,
+    };
+    randomNumbers = Array
+        .from({ length: this.props.randomNumberCount })
+        .map(() => 1 + Math.floor(10 * Math.random()));
+        target = this.randomNumbers
+        .slice(0, this.props.randomNumberCount - 2)
+        .reduce((acc, curr) => acc + curr, 0);
     render() {
         return (
             <View style={styles.container}>
                 <Text style={styles.target}>{this.target}</Text>
+                {this.randomNumbers.map((randomNumber, idex) =>
+                <Text>{randomNumber}</Text>
+                )}
             </View>
         );
     }
@@ -29,11 +41,11 @@ const styles = StyleSheet.create({
     },
 
 
-target: {
-    fontSize:40,
-    backgroundColor: '#aaa',
-    marginHorizontal: 50,
-    textAlign: 'center',
-},
+    target: {
+        fontSize: 40,
+        backgroundColor: '#aaa',
+        marginHorizontal: 50,
+        textAlign: 'center',
+    },
 });
 export default Game;
