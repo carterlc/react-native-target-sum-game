717a4f4d0f54509b705dd49538e1f198c1cd7e42
Carter Clifford <carterlc@live.ca>
2023-02-28 15:19:15 -0600

    Made the UI change color when the user wins or loses the game. Also stops the user from playing the game if the game is decided.

--- Game.js
+++ Game.js
@@ -50,18 +50,19 @@ class Game extends React.Component {
         }
         // console.warn(sumSelected);
     };
+
     render() {
         const gameStatus = this.gameStatus();
         return (
             <View style={styles.container}>
-                <Text style={styles.target}>{this.target}</Text>
+                <Text style={[styles.target, styles[`STATUS_${gameStatus}`]]}>{this.target}</Text>
                 <View style={styles.randomContainer}>
                     {this.randomNumbers.map((randomNumber, index) =>
                         <RandomNumber 
                             key={index}
                             id={index}
                             number={randomNumber}
-                            isDisabled={this.isNumberSelected(index)}
+                            isDisabled={this.isNumberSelected(index) || gameStatus !== 'PLAYING'}
                             onPress={this.selectNumber}
                             />
                     )}
@@ -96,6 +97,16 @@ const styles = StyleSheet.create({
         flexWrap: 'wrap',
         justifyContent: 'space-around',
         marginHorizontal: 20,
+    },
+
+    STATUS_PLAYING: {
+        backgroundColor:'white',
+    },
+    STATUS_WON: {
+        backgroundColor:'green',
+    },
+    STATUS_LOST: {
+        backgroundColor:'red',
     },
 });
 export default Game;
